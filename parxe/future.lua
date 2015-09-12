37b634afadf42dc8b9f4e3f9876a51d58f67b1ad
Paco Zamora Martinez <pakozm@gmail.com>
2015-09-12 16:42:25 +0200

    Forcing future check after every operation of new future construction

--- parxe/future.lua
+++ parxe/future.lua
@@ -231,6 +231,7 @@ function future.all(tbl)
   f.get_stdout = all_get_stdout
   f.get_stderr = all_get_stderr
   f.data = tbl
+  f:ready()
   return f
 end
 
@@ -258,6 +259,7 @@ function future.conditioned(func, other, ...)
   f.func = func
   f.data = other
   f.args = table.pack(...)
+  f:ready()
   return f
 end
 
